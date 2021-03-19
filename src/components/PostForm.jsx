eea94d9534ea5d5ad8ee264637f243929a73b6db
Mukaila Semiu <semiteagain@gmail.com>
2021-03-19 05:58:17 +0100

    Updating post with useState

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function PostForm() {
+
+    const [item, setItems] = useState({
+        title : '',
+        body : ''
+    })
+
+     function handleChange(e) {
+         const {name, value} = e.target
+         setItems((prevItem) =>{
+             return {
+                 ...prevItem,
+                 [name] : value
+             }
+         })
+     }
+
+
+
     return (
         <div>
             <h1>Add post</h1>
             <form>
                 <div>
                     <label htmlFor="">Title</label> <br/>
-                <input type='text' name='title' />
+                <input type='text' name='title' value={item.title} onChange={handleChange} />
                 </div>
                
                 <br/>
                 <div>
                     <label htmlFor="">Body</label> <br/>
-                <textarea type='text' name='body' />
+                <textarea type='text' name='body' value={item.body} onChange={handleChange} />
                 </div>
                 <br/>
                 <button type="submit">Submit</button>
